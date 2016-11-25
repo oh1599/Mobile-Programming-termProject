cd2b3791b910302fccd7c58c2ef2976fbdc5bef2
Hansam <oh1599@kookmin.ac.kr>
2016-11-26 01:18:45 +0900

    메인 화면 레이아웃 완성

--- MyLifeLogger/app/src/main/java/com/example/oh/mylifelogger/MainActivity.java
+++ MyLifeLogger/app/src/main/java/com/example/oh/mylifelogger/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.oh.mylifelogger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void write(View view)
+    {
+        intent=new Intent(this,WriteActivity.class);
+        startActivity(intent);
     }
 }
---
+++ MyLifeLogger/app/src/main/java/com/example/oh/mylifelogger/WriteActivity.java
@@ -0,0 +1,15 @@
+package com.example.oh.mylifelogger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Oh on 2016-11-26.
+ */
+
+public class WriteActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
