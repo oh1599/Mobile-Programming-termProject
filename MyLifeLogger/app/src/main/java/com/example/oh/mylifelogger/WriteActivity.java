f25f96d270aa716e38d66e92b0fa47f916c1d8ec
Hansam <oh1599@kookmin.ac.kr>
2016-11-29 21:07:44 +0900

    글쓰기 레이아웃 완성

--- MyLifeLogger/app/src/main/java/com/example/oh/mylifelogger/WriteActivity.java
+++ MyLifeLogger/app/src/main/java/com/example/oh/mylifelogger/WriteActivity.java
@@ -2,6 +2,9 @@ package com.example.oh.mylifelogger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        Spinner s = (Spinner)findViewById(R.id.thema);
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
 }
